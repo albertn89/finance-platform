75db4ad72fed0c831de119e6f34f280ea9ffcaba
Albert Negoro <albert.negoro89@gmail.com>
2024-08-27 16:05:06 -0700

    Dash header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Finance",
@@ -18,7 +18,7 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={inter.className}>{children}</body>
+				<body className={font.className}>{children}</body>
 			</html>
 		</ClerkProvider>
 	);
