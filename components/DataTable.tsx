da7b4dc3d99a16a11a463459a6f763115a903ba8
Albert Negoro <albert.negoro89@gmail.com>
2024-09-04 21:49:25 -0700

    Bulk delete accounts

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -16,6 +16,8 @@ import {
 	useReactTable,
 } from "@tanstack/react-table";
 
+import { useConfirm } from "@/hooks/use-confirm";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -42,6 +44,11 @@ export function DataTable<TData, TValue>({
 	onDelete,
 	disabled,
 }: DataTableProps<TData, TValue>) {
+	const [ConfirmDialog, confirm] = useConfirm(
+		"Are you sure?",
+		"You are about to perform a bulk delete."
+	);
+
 	const [sorting, setSorting] = React.useState<SortingState>([]);
 	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
 	const [rowSelection, setRowSelection] = React.useState({});
@@ -49,9 +56,9 @@ export function DataTable<TData, TValue>({
 	const table = useReactTable({
 		data,
 		columns,
-		onSortingChange: setSorting,
 		getCoreRowModel: getCoreRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
+		onSortingChange: setSorting,
 		getSortedRowModel: getSortedRowModel(),
 		onColumnFiltersChange: setColumnFilters,
 		getFilteredRowModel: getFilteredRowModel(),
@@ -65,11 +72,12 @@ export function DataTable<TData, TValue>({
 
 	return (
 		<div>
+			<ConfirmDialog />
 			<div className="flex items-center py-4">
 				<Input
 					placeholder={`Filter ${filterKey}...`}
-					value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-					onChange={(event) => table.getColumn("email")?.setFilterValue(event.target.value)}
+					value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
+					onChange={(event) => table.getColumn(filterKey)?.setFilterValue(event.target.value)}
 					className="max-w-sm"
 				/>
 				{table.getFilteredSelectedRowModel().rows.length > 0 && (
@@ -78,6 +86,14 @@ export function DataTable<TData, TValue>({
 						size="sm"
 						variant="outline"
 						className="ml-auto font-normal text-xs"
+						onClick={async () => {
+							const ok = await confirm();
+
+							if (ok) {
+								onDelete(table.getFilteredSelectedRowModel().rows);
+								table.resetRowSelection();
+							}
+						}}
 					>
 						<Trash className="size-4 mr-2" />
 						Delete ({table.getFilteredSelectedRowModel().rows.length})
